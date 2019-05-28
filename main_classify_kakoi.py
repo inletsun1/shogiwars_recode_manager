42102eb78acfc914c07e1086658fc305e9896297
Yoichi Irie <yoichi.m.i430@gmail.com>
2019-05-28 21:34:35 +0900

    分類した戦型に保存

--- main_classify_kakoi.py
+++ main_classify_kakoi.py
@@ -1,16 +1,29 @@
 from classify_strategy import *
 import glob 
+import os
+import shutil
 
 
 kifu_dir = './kifu/*.csa'
 files = glob.glob(kifu_dir)
+kakoi_files = glob.glob('./kakoi/*.csa')
+
+def mkdir(path):
+    if not os.path.isdir(path):
+        os.makedirs(path)
+
+print("start")
 for filename in files:
-    kakoi = kakoi2string('./kakoi/yokofu.csa')
-    print(filename)
-    tmp_ban, sashite = kifu2string(filename)
-    if isMatchKakoi(tmp_ban, sashite, kakoi):
-        print('横歩取り')
-    else:
-        print('力戦')
+    for kakoi_name in kakoi_files:
+        kakoi = kakoi2string(kakoi_name)
+        tmp_ban, sashite = kifu2string(filename)
+        if isMatchKakoi(tmp_ban, sashite, kakoi):
+            dirname = kakoi_name.split("/")
+            dirname = dirname[2]
+            dirname = './' + dirname[0:-4] + "/"
+            mkdir(dirname)
+            fn = filename.split("/")
+            shutil.copy(filename, dirname + fn[-1])
+print("finished")
 
 rev_kakoi = reverseSenteGote(kakoi)
